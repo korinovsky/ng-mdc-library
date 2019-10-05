7d38f1d696132947f0904655088ebcbbe54c6934
Konstantin Korinovsky <k.korinovsky@gmail.com>
2019-10-05 14:51:46 +0300

    Refactoring

--- projects/mdc/src/lib/dialog/dialog.component.ts
+++ projects/mdc/src/lib/dialog/dialog.component.ts
@@ -12,7 +12,7 @@ import {closest, matches} from '@material/dom/ponyfill';
 import {DialogTitleDirective} from './title/title.directive';
 
 const LAYOUT_EVENTS = ['resize', 'orientationchange'];
-let id = 1;
+let id = 0;
 
 @Component({
   selector: 'mdc-dialog',
@@ -27,9 +27,9 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
   @ContentChild(DialogTitleDirective, {static: false}) title: DialogTitleDirective;
   @ContentChild(DialogContentDirective, {static: false}) content: DialogContentDirective;
   @ContentChildren(DialogButtonComponent, {descendants: true}) buttonQueryList: QueryList<DialogButtonComponent>;
+  componentId = ++id;
   private focusTrap: FocusTrap;
   private foundation: MDCDialogFoundation;
-  id = id++;
 
   get dialogElement(): HTMLElement {
     return this.dialog.nativeElement as HTMLElement;
@@ -37,6 +37,8 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
 
   private get adapter(): MDCDialogAdapter {
     const initialFocusAttribute = MDCDialogFoundation.strings.INITIAL_FOCUS_ATTRIBUTE;
+    const actionAttribute = MDCDialogFoundation.strings.ACTION_ATTRIBUTE;
+    const buttonDefaultAttribute = MDCDialogFoundation.strings.BUTTON_DEFAULT_ATTRIBUTE;
     return {
       addClass: (className: string) => this.dialogElement.classList.add(className),
       removeClass: (className: string) => this.dialogElement.classList.remove(className),
@@ -44,13 +46,32 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
       addBodyClass: (className: string) => document.body.classList.add(className),
       removeBodyClass: (className: string) => document.body.classList.remove(className),
       eventTargetMatches: (target: HTMLElement, selector: string) => matches(target, selector),
-      trapFocus: (focusElement: HTMLElement) => this.trapFocus(focusElement),
+      trapFocus: (focusElement: HTMLElement) => {
+        this.focusTrap = createFocusTrapInstance(this.dialogElement, undefined, focusElement);
+        this.focusTrap.activate();
+      },
       releaseFocus: () => this.focusTrap && this.focusTrap.deactivate(),
       isContentScrollable: () => this.content ? isScrollable(this.content.nativeElement) : false,
-      areButtonsStacked: () => this.areButtonsStacked(),
-      getActionFromEvent: (event: any) => this.getActionFromEvent(event),
-      clickDefaultButton: () => this.clickDefaultButton(),
-      reverseButtons: () => this.getReverseButtons(),
+      areButtonsStacked: () => {
+        const buttons = this.buttons;
+        return buttons.length ? areTopsMisaligned(buttons) : false;
+      },
+      getActionFromEvent: (event: any) => {
+        const elem = closest(event.target, `[${actionAttribute}]`);
+        return elem && elem.getAttribute(actionAttribute);
+      },
+      clickDefaultButton: () => {
+        const button = this.dialogElement.querySelector(`[${buttonDefaultAttribute}]`) as HTMLButtonElement;
+        if (button) {
+          button.click();
+        }
+      },
+      reverseButtons: () => {
+        const buttons = this.buttons;
+        return buttons.length && buttons
+          .reverse()
+          .forEach(button => button.parentElement && button.parentElement.appendChild(button));
+      },
       notifyOpening: () => this.handleOpening(),
       notifyOpened: () => this.opened.emit(),
       notifyClosing: (action: string) => this.handleClosing(action),
@@ -63,24 +84,14 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
     return this.buttonQueryList.map(item => item.nativeElement);
   }
 
-  @HostListener('click', ['$event'])
-  private handleClick(event): void {
-    this.foundation.handleClick(event);
-  }
-
-  @HostListener('keydown', ['$event'])
-  private handleKeydown(event): void {
-    this.foundation.handleKeydown(event);
-  }
-
   ngAfterContentInit(): void {
     if (this.content) {
-      const contentId = `mdc-dialog-content-${this.id}`;
+      const contentId = `mdc-dialog-content-${this.componentId}`;
       this.content.nativeElement.setAttribute('id', contentId);
       this.dialogElement.setAttribute('aria-describedby', contentId);
     }
     if (this.title) {
-      const titleId = `mdc-dialog-title-${this.id}`;
+      const titleId = `mdc-dialog-title-${this.componentId}`;
       this.title.nativeElement.setAttribute('id', titleId);
       this.dialogElement.setAttribute('aria-labelledby', titleId);
     }
@@ -100,38 +111,17 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
     this.foundation.close(action);
   }
 
-  private getActionFromEvent(event: any) {
-    const actionAttribute = MDCDialogFoundation.strings.ACTION_ATTRIBUTE;
-    const elem = closest(event.target, `[${actionAttribute}]`);
-    return elem && elem.getAttribute(actionAttribute);
+  @HostListener('click', ['$event'])
+  private handleClick(event): void {
+    this.foundation.handleClick(event);
   }
 
-  private trapFocus(focusElement: HTMLElement) {
-    this.focusTrap = createFocusTrapInstance(this.dialogElement, undefined, focusElement);
-    this.focusTrap.activate();
+  @HostListener('keydown', ['$event'])
+  private handleKeydown(event): void {
+    this.foundation.handleKeydown(event);
   }
 
-  private areButtonsStacked() {
-    const buttons = this.buttons;
-    return buttons.length ? areTopsMisaligned(buttons) : false;
-  }
-
-  private clickDefaultButton() {
-    const buttonDefaultAttribute = MDCDialogFoundation.strings.BUTTON_DEFAULT_ATTRIBUTE;
-    const button = this.dialogElement.querySelector(`[${buttonDefaultAttribute}]`) as HTMLButtonElement;
-    if (button) {
-      button.click();
-    }
-  }
-
-  private getReverseButtons() {
-    const buttons = this.buttons;
-    return buttons.length && buttons
-      .reverse()
-      .forEach(button => button.parentElement && button.parentElement.appendChild(button));
-  }
-
-  private handleOpening() {
+  private handleOpening(): void {
     this.opening.emit();
     LAYOUT_EVENTS.forEach((event: string) =>
       window.addEventListener(event, this.handleLayout.bind(this))
@@ -139,7 +129,7 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
     document.addEventListener('keydown', this.handleDocumentKeyDown.bind(this));
   }
 
-  private handleClosing(action: string) {
+  private handleClosing(action: string): void {
     this.closing.emit(action);
     LAYOUT_EVENTS.forEach((event: string) =>
       window.removeEventListener(event, this.handleLayout.bind(this))
@@ -147,11 +137,11 @@ export class DialogComponent implements AfterContentInit, OnDestroy {
     document.removeEventListener('keydown', this.handleDocumentKeyDown.bind(this));
   }
 
-  private handleDocumentKeyDown(e: KeyboardEvent) {
+  private handleDocumentKeyDown(e: KeyboardEvent): void {
     this.foundation.handleDocumentKeydown(e);
   }
 
-  private handleLayout() {
-    return this.foundation.layout();
+  private handleLayout(): void {
+    this.foundation.layout();
   }
 }
