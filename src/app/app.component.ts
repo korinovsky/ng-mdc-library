e109f050892aab18965553def3bd591fa55dbf29
Konstantin Korinovsky <k.korinovsky@gmail.com>
2019-10-12 09:32:32 +0300

    Button disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import {Component} from '@angular/core';
-import {DialogComponent} from '@mdc/dialog/dialog.component';
-import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  openDialog(dialog: DialogComponent) {
-    dialog.open();
-    dialog.closed
-      .pipe(first())
-      .subscribe(action => console.log(action));
-  }
-}
+export class AppComponent {}
